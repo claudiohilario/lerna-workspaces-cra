792eba4db9f1050c1ebfb3eea20ca4b66f402dcc
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-11 15:11:18 +0100

    [ADD] Scss Shared Components

--- packages/shared/Button.js
+++ packages/shared/Button.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Button.module.css";
+import styles from "./Button.module.scss";
 export const Button = props => (
     <button className={styles.button} onClick={props.onClick} {...props}>
         {props.children}
